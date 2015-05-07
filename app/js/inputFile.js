abe552bf73f2b20dfb5cf64febe786fc880876ed
Asya Gai <asyagai@gmail.com>
2015-05-07 14:33:43 +0300

    validation popup

--- app/js/inputFile.js
+++ app/js/inputFile.js
@@ -1,14 +1,20 @@
-"use strict";
+function Bpopup() {
+    var el;
+    el = document.getElementById("addProgect");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
 
-var validation = (function () {
+var validation = (function() {
+
     var init = function() {
-            console.log('Инициализация модуля Validation');
+            console.log('????????????? ?????? Validation');
             _setUpListners();
         },
-        validateForm = function(form) {
-            console.log('Проверка формы');
 
-            var elements = form.find('.dz4-input-file-name'),
+        validateForm = function(form) {
+            console.log('???????? ?????');
+
+            var elements = form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
                 valid = true;
 
             $.each(elements, function(index,val) {
@@ -16,7 +22,7 @@ var validation = (function () {
                     val = element.val(),
                     pos = element.attr('qtip-position');
 
-                if(val.length === 0) {
+                if(val.length === 0 ) {
                     element.addClass('dz4-form__input--error');
                     _createQtip(element,pos);
                     valid = false;
@@ -26,18 +32,29 @@ var validation = (function () {
         },
         _setUpListners = function(){
             $('form').on('keydown','.dz4-form__input--error', _removeError);
+            $('form').on('change', '#file', _removeErrorUpload);
             $('form').on('reset', _clearForm);
         },
+
         _removeError = function() {
-            console.log('Удаление красной обводки');
+            console.log('???????? ??????? ???????');
             $(this).removeClass('dz4-form__input--error');
         },
+
         _clearForm = function(form) {
             var form = $(this);
             form.find('input, textarea').trigger('hideTooltip');
             form.find('.dz4-form__input--error').removeClass('dz4-form__input--error');
             form.find('.dz6-form--info__success, .dz6-form--info__error').text('').hide();
         },
+
+    // remove highlight and qtip from upload field
+        _removeErrorUpload = function() {
+            var filename = $('#file-name');
+            filename.removeClass('dz4-form__input--error');
+            filename.trigger('hideTooltip');
+        },
+
         _createQtip = function(element, position) {
             console.log('tooltip');
 
@@ -78,9 +95,71 @@ var validation = (function () {
                 }
             }).trigger('show');
         };
+
     return {
         init: init,
         validateForm: validateForm
-    };
+    }
+
 })();
-validation.init();
+
+
+// 
+//fix for fake upload placeholder
+// 
+var UploadFix = (function() {
+    // Setting up listening if user has chose a file
+    var _setUpListeners = function() {
+            $('#file').on('change', _showPlaceholder);
+        },
+
+    // adding a placeholder text
+        _showPlaceholder = function(ev) {
+            ev.preventDefault();
+
+            var realVal = $(this).val(),
+                lastIndex = realVal.lastIndexOf('\\') + 1;
+
+            if (lastIndex !== -1) {
+                realVal = realVal.substr(lastIndex);
+                $('#file-name').val(realVal);
+            }
+        }
+
+    return {
+        init: _setUpListeners
+    }
+
+})();
+
+// 
+// fixes for placeholder in ie
+// 
+var FixPlaceholders = (function() {
+    var _fixPlaceholders = function() {
+        $('input, textarea').placeholder();
+    }
+
+    return {
+        init: function() {
+            _fixPlaceholders();
+        }
+    }
+})();
+
+$(document).ready(function() {
+
+    if ($.find('#file-form-label').length > 0) {
+        UploadFix.init();
+    }
+
+    if ($.find('.projects-item').length > 0) {
+        Projects.init();
+    }
+
+
+    if ($.find('form').length > 0) {
+        validation.init();
+        FixPlaceholders.init();
+    }
+});
